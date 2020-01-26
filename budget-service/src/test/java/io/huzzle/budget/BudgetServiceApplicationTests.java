548bbae7606535118ac870c6082f4419cec6563a
Selvakumar Esra <selva@typesafe.me>
2020-01-26 20:26:11 +0800

    microservices initial commit

---
+++ budget-service/src/test/java/io/huzzle/budget/BudgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.huzzle.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
