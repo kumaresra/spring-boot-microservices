5b7974bfa70ed0f8b678530ddbfd86271667c133
Selvakumar Esra <selva@typesafe.me>
2020-01-26 20:27:55 +0800

    microservices initial commit

---
+++ portfolio-service/src/test/java/io/huzzle/portfolio/PortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.huzzle.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
