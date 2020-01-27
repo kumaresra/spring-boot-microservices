85f551956ae8dd906bef0fd7daa14f17bf3ee527
Selvakumar Esra <selva@typesafe.me>
2020-01-27 09:53:04 +0800

    Spring boot best practices

--- portfolio-service/src/main/java/io/huzzle/portfolio/PortfolioServiceApplication.java
+++ portfolio-service/src/main/java/io/huzzle/portfolio/PortfolioServiceApplication.java
@@ -2,12 +2,35 @@ package io.huzzle.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableSwagger2
 public class PortfolioServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioServiceApplication.class, args);
+	}
+
+	@Bean
+	public Docket swaggerApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("io.huzzle.portfolio.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(new ApiInfoBuilder().version("1.0").title("Portfolio API").
+						description("Documentation Portfolio API v1.0").build());
 	}
 
 }
--- proxy-service/src/main/java/io/huzzle/proxy/ProxyServiceApplication.java
+++ proxy-service/src/main/java/io/huzzle/proxy/ProxyServiceApplication.java
@@ -2,8 +2,12 @@ package io.huzzle.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableSwagger2
 public class ProxyServiceApplication {
 
 	public static void main(String[] args) {
